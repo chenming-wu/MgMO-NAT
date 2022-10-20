82ad916594edd0250d3179a83cd06876fce7d0b5
Chenming Wu <wcm1994@qq.com>
2022-10-21 00:59:42 +0800

    rollback

--- requirement.sh
+++ requirement.sh
@@ -1,4 +1,4 @@
-pip install tensorflow==2.3 tensorboard sacremoses nltk Ninja omegaconf
+pip install tensorflow tensorboard sacremoses nltk Ninja omegaconf
 pip install 'fuzzywuzzy[speedup]'
 pip install hydra-core==1.0.6
 pip install sacrebleu==1.5.1
